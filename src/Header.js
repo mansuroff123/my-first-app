34354b5dd5ed97ee0d368d121cdadb2ff0d281f2
mansuroff123 <mansuroff145@gmail.com>
2025-06-14 19:57:56 +0500

    Header and Main App js files done

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Header() {
+    return (
+        <header>
+            <h2>It's Header section</h2>
+        </header>
+    );
+}
+
+export default Header;
