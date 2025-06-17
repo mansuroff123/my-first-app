5b999a96c94342da04fd56d8b4b35e6c9b32e806
mansuroff123 <mansuroff145@gmail.com>
2025-06-17 19:58:31 +0500

    react components explained

---
+++ src/components/Heading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Heading() {
+    return (
+        <header>
+            <h2>Hojiakbar project</h2>
+            <h2>My Favourite Foods</h2>
+        </header>
+    );
+}
+
+export default Heading;
---
+++ src/components/List.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function List() {
+    return (
+        <div>
+            <ul>
+                <li>Bacon</li>
+                <li>Jamon</li>
+                <li>Sushi</li>
+            </ul>
+        </div>
+    )
+}
+
+export default List;
